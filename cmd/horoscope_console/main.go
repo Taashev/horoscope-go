46df55c8c3abd13cdc448af821a74395de3b3787
Taashev <taashev.a.r92@yandex.ru>
2024-03-05 06:42:29 +0300

    DEV-2: calculate year

--- cmd/horoscope_console/main.go
+++ cmd/horoscope_console/main.go
@@ -2,9 +2,11 @@ package run
 
 import (
 	getName "horoscope_console/internal/get_name"
+	greeting "horoscope_console/internal/greeting"
 )
 
 func Run() {
 	name := getName.GetName()
 
+	greeting.PrintGreeting(name)
 }
---
+++ internal/greeting/greeting.go
@@ -0,0 +1,7 @@
+package greeting
+
+import "fmt"
+
+func PrintGreeting(name string) {
+	fmt.Printf("Привет, %s!\n", name)
+}
